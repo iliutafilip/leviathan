c4d9d3972e4a9676dd1c9c9d03ae2cc92e3cdf77
iliutafilip <iliuta_filip@yahoo.com>
2025-03-17 20:05:04 +0200

    -

--- LLM/LLM_integration.py
+++ LLM/LLM_integration.py
@@ -4,14 +4,12 @@ from typing import List, Optional
 import requests
 import yaml
 
-
 with open("configs/plugins-config.yaml") as file:
     config = yaml.safe_load(file)
 
-
 LLM_PROVIDER = config.get("plugin").get("llmProvider")
 LLM_MODEL = config.get("plugin").get("llmModel")
-OPENAI_SECRET_KEY = config.get("plugin").get("openAISecretKey")
+API_SECRET_KEY = config.get("plugin").get("openAISecretKey")
 
 
 class Role(Enum):
@@ -22,7 +20,9 @@ class Role(Enum):
 
 class LLMProvider(Enum):
     OPENAI = "openai"
+    DEEPSEEK = "deepseek"
     INVALID_PROVIDER = "invalid_provider"
+
 
 class Message:
     def __init__(self, role: Role, content: str):
@@ -40,49 +40,49 @@ class LLMHoneypot:
         return f"""
             You are an Ubuntu Linux terminal. Respond with the exact command output, using '\r\n' for new lines, WITHOUT EXCEPTIONS.  
             NO Markdown, explanations, or extra comments — just the raw terminal output.
-            
+
             Format responses as:  
             `<username>@<ssh_server_ip>:<current_directory>$ `  
-            
+
             - `~` represents "/home/<username>/".  
-            - Adjust `<current_directory>` when using `cd`/
-            
+            - Adjust `<current_directory>` when using `cd`
+
             Strict Formatting Rules:
             - Use ONLY `\r\n` for new lines—never `\n` alone.
             - NO extra blank lines before or after output.
             - If a command has no output, return ONLY the new prompt.
-            
+
             User: {username}  
             Server: {ssh_server_ip}
         """
 
-
     OPENAI_ENDPOINT = "https://api.openai.com/v1/chat/completions"
+    DEEPSEEK_ENDPOINT = "https://api.deepseek.com/v1/chat/completions"
+
 
     def __init__(
             self,
             username: str = "unknown",
             ssh_server_ip: str = "127.0.0.1",
-            host: Optional[str] = None,
             custom_prompt: Optional[str] = None,
     ):
         self.provider = LLMProvider(LLM_PROVIDER.lower())
         self.model = LLM_MODEL
-        self.openai_key = OPENAI_SECRET_KEY
+        self.api_key = API_SECRET_KEY
         self.username = username
         self.ssh_server_ip = ssh_server_ip
-        self.host = host or (
-            self.OPENAI_ENDPOINT if self.provider == LLMProvider.OPENAI else LLMProvider.INVALID_PROVIDER)
         self.custom_prompt = custom_prompt
         self.session = requests.Session()
 
-        message = Message(Role.SYSTEM, self._get_system_prompt(self.username, self.ssh_server_ip) if not self.custom_prompt else self.custom_prompt)
+        self.api_endpoint = self.OPENAI_ENDPOINT if self.provider == LLMProvider.OPENAI else self.DEEPSEEK_ENDPOINT
+
+        message = Message(Role.SYSTEM, self._get_system_prompt(self.username,
+                                                               self.ssh_server_ip) if not self.custom_prompt else self.custom_prompt)
         self.histories: List[Message] = [message]
 
         example_interactions = [
-
             Message(Role.USER, "ls"),
-            Message(Role.ASSISTANT, f"Desktop  Documents  Downloads  Music  Pictures  Videos\r\n{self.username}@{self.ssh_server_ip}:~$ "),
+            Message(Role.ASSISTANT,f"Desktop  Documents  Downloads  Music  Pictures  Videos\r\n{self.username}@{self.ssh_server_ip}:~$ "),
         ]
 
         self.histories.extend(example_interactions)
@@ -97,22 +97,33 @@ class LLMHoneypot:
     def execute_model(self, command: str) -> str:
         messages = self.build_prompt(command)
 
-        if self.provider == LLMProvider.OPENAI:
-            response = self._openai_caller(messages)
+        if self.provider == LLMProvider.OPENAI or self.provider == LLMProvider.DEEPSEEK:
+            response = self._api_caller(messages)
         else:
             raise ValueError("Invalid LLM Provider")
 
         self.histories.append(Message(Role.USER, command))
         self.histories.append(Message(Role.ASSISTANT, response))
 
-        # TODO: save input and output for fine tuning
-
         return response
 
-    def _openai_caller(self, messages: List[Message]) -> str:
-        if not self.openai_key:
-            raise ValueError("OpenAI key is required")
+    def _api_caller(self, messages: List[Message]):
+        if not self.api_key:
+            raise ValueError("API key is required")
 
+        payload, headers = self._create_payload(messages)
+
+        response = self.session.post(self.api_endpoint, json=payload, headers=headers)
+        response_data = response.json()
+
+        if "choices" not in response_data or not response_data["choices"]:
+            raise ValueError("No choices returned from LLM provider")
+
+        final_response = self._remove_quotes(response_data["choices"][0]["message"]["content"])
+        return final_response
+
+
+    def _create_payload(self, messages: List[Message]):
         payload = {
             "model": self.model,
             "messages": [msg.to_dict() for msg in messages],
@@ -121,18 +132,10 @@ class LLMHoneypot:
 
         headers = {
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {self.openai_key}",
+            "Authorization": f"Bearer {self.api_key}",
         }
 
-        response = self.session.post(self.host, json=payload, headers=headers)
-        response_data = response.json()
-
-        if "choices" not in response_data or not response_data["choices"]:
-            raise ValueError("No choices returned from OpenAI")
-
-        final_response = self._remove_quotes(response_data["choices"][0]["message"]["content"])
-
-        return final_response
+        return payload, headers
 
 
     @staticmethod
